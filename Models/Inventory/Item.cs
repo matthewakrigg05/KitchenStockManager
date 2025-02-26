f7a048cd8a6e37670fedfa53992aae828ed9e631
Matthew Akrigg <matthewakrigg05@gmail.com>
2025-02-26 19:31:31 +0000

    feat: item class and constructors

--- Models/Inventory/Item.cs
+++ Models/Inventory/Item.cs
@@ -4,10 +4,36 @@ namespace KitchenStockManager.Models.Inventory
 {
     internal class Item
     {
-        private string name;
-        private int quantity;
-        private string unit;
-        private float cost;
-        private Supplier supplier;
+        private string name { get; set; }
+        private int quantity { get; set; }
+        private string unit { get; set; }
+        private float cost { get; set; }
+        private Supplier supplier { get; set; }
+    
+        public Item()
+        {
+            name = null;
+            quantity = 0;
+            unit = null;
+            cost = 0;
+            supplier = null;
+        }
+
+        public Item(string itemName, int itemQuantity, string unitType, float itemCost)
+        {
+            name = itemName;
+            quantity = itemQuantity;
+            unit = unitType;
+            cost = itemCost;
+        }
+
+        public Item(string itemName, int itemQuantity, string unitType, float itemCost, Supplier itemSupplier)
+        {
+            name = itemName;
+            quantity = itemQuantity;
+            unit = unitType;
+            cost = itemCost;
+            supplier = itemSupplier;
+        }
     }
 }
