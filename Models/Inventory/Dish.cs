337503b06635d77944a5d2406002608824674a72
Matthew Akrigg <matthewakrigg05@gmail.com>
2025-02-26 19:50:03 +0000

    feat: dish class and constructors

--- Models/Inventory/Dish.cs
+++ Models/Inventory/Dish.cs
@@ -1,6 +1,32 @@
 ﻿namespace KitchenStockManager.Models.Inventory
 {
-    internal class Dish
+    class Dish
     {
+        private string name {  get; set; }
+        private List<RawIngredient> rawIngredients {  get; set; }
+        private List<PreparedIngredient> preparedIngredients { get; set; }
+        private string description {  get; set; }
+
+        public Dish() { }
+
+        public Dish(string dishName)
+        {
+            name = dishName;
+        }
+
+        public Dish(string dishName, string dishDesc)
+        {
+            name = dishName;
+            description = dishDesc;
+        }
+
+        public Dish(string dishName, string dishDesc, List<RawIngredient> rawIngreds, List<PreparedIngredient> preparedIngreds)
+        {
+            name = dishName;
+            description = dishDesc;
+            rawIngredients = rawIngreds;
+            preparedIngredients = preparedIngreds;
+        }
+
     }
 }
