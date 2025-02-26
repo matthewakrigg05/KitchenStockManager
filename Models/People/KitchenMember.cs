249287750bd76e1cffa9ee29200a08e4c309e548
Matthew Akrigg <matthewakrigg05@gmail.com>
2025-02-26 22:53:31 +0000

    feat: kitchen member class and constructor

--- Models/People/KitchenMember.cs
+++ Models/People/KitchenMember.cs
@@ -2,5 +2,14 @@
 {
     class KitchenMember : Staff
     {
+        private string kitchenRole { get; set; }
+
+        public KitchenMember() { }
+
+        public KitchenMember(string firstName, string lastName, string role, string contactNumber, string email, string memberRole)
+            : base(firstName, lastName, role, contactNumber, email)
+        {
+            kitchenRole = memberRole;
+        }
     }
 }
