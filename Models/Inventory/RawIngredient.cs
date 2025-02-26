a86d5041d206f2da9a12795154d6bc9f180d2e46
Matthew Akrigg <matthewakrigg05@gmail.com>
2025-02-26 19:45:25 +0000

    feat: raw ingredient constructors

--- Models/Inventory/RawIngredient.cs
+++ Models/Inventory/RawIngredient.cs
@@ -1,8 +1,16 @@
-﻿namespace KitchenStockManager.Models.Inventory
+﻿using KitchenStockManager.Models.People;
+
+namespace KitchenStockManager.Models.Inventory
 {
     class RawIngredient : Item
     {
+        public RawIngredient() { }
 
+        public RawIngredient(string itemName, int itemQuantity, string unitType, float itemCost)
+            : base(itemName, itemQuantity, unitType, itemCost) { }
+
+        public RawIngredient(string itemName, int itemQuantity, string unitType, float itemCost, Supplier itemSupplier)
+            : base(itemName, itemQuantity, unitType, itemCost, itemSupplier) { }
 
     }
 }
