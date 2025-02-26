7331284605435762d199774ac663c0bd3a972e2e
Matthew Akrigg <matthewakrigg05@gmail.com>
2025-02-26 19:42:46 +0000

    feat: updated prepared ingredients constructors - some missed

--- Models/Inventory/PreparedIngredient.cs
+++ Models/Inventory/PreparedIngredient.cs
@@ -1,9 +1,12 @@
-﻿namespace KitchenStockManager.Models.Inventory
+﻿using KitchenStockManager.Models.Other;
+
+namespace KitchenStockManager.Models.Inventory
 {
     class PreparedIngredient : Item
     {
         private string preparationInstructions { get; set; }
         private List<RawIngredient> ingredients {  get; set; }
+        private List<PreparedIngredient> preparedIngredients { get; set; }
 
         public PreparedIngredient() { }
 
@@ -11,6 +14,19 @@
         {
             preparationInstructions = preparationInstructs;
             ingredients = ingreds;
+        }
+
+        public PreparedIngredient(string preparationInstructs, List<PreparedIngredient> ingreds)
+        {
+            preparationInstructions = preparationInstructs;
+            preparedIngredients = ingreds;
+        }
+
+        public PreparedIngredient(string preparationInstructs, List<RawIngredient> ingreds, List<PreparedIngredient> preparedIngreds)
+        {
+            preparationInstructions = preparationInstructs;
+            ingredients = ingreds;
+            preparedIngredients = preparedIngredients;
         }
     }
 }
