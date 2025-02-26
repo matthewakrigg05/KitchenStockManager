81e514e279921cb6156155c94e2c4919ee4c9442
Matthew Akrigg <matthewakrigg05@gmail.com>
2025-02-26 08:21:37 +0000

    feat: initial person class

---
+++ people/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace KitchenStockManager.people
+{
+    class Person
+    {
+        private string firstName;
+        private string lastName;
+        private string role;
+        private string contactNumber;
+    }
+}
