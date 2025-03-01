f022cbeaceafa5746fdd5cd84d6bf7a4b22dacf5
Matthew Akrigg <matthewakrigg05@gmail.com>
2025-03-01 14:52:53 +0000

    feat: recipe gets/sets

--- Models/Inventory/Recipe.cs
+++ Models/Inventory/Recipe.cs
@@ -15,5 +15,53 @@
             rawIngredients = rawIngreds;
         }
 
+        public Recipe(string recipeInstructions, List<PreparedIngredient> prepIngreds)
+        {
+            instructions = recipeInstructions;
+            preparedIngredients = prepIngreds;
+        }
+
+        public Recipe(string recipeInstructions, List<RawIngredient> rawIngreds)
+        {
+            instructions = recipeInstructions;
+            rawIngredients = rawIngreds;
+        }
+
+        public Recipe(string recipeInstructions)
+        {
+            instructions = recipeInstructions;
+        }
+
+        // Getters and setters - no other methods beyond this point
+
+        public void SetInstructions(string recipeInstructions)
+        {
+            instructions = recipeInstructions;
+        }
+
+        public string GetInstructions()
+        {
+            return instructions;
+        }
+
+        public void SetPreparedIngredients(List<PreparedIngredient> prepIngreds)
+        {
+            preparedIngredients = prepIngreds;
+        }
+
+        public List<PreparedIngredient> GetPreparedIngredients()
+        {
+            return preparedIngredients;
+        }
+
+        public void SetRawIngredients(List<RawIngredient> rawIngreds)
+        {
+            rawIngredients = rawIngreds;
+        }
+
+        public List<RawIngredient> GetRawIngredients()
+        {
+            return rawIngredients;
+        }
     }
 }
