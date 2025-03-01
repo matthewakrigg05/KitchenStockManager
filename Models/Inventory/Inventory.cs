3ec21e2eba71ede840d5fe4452776cefdaf0c713
Matthew Akrigg <matthewakrigg05@gmail.com>
2025-03-01 14:51:48 +0000

    feat: item gets and sets, and some simple methods

--- Models/Inventory/Inventory.cs
+++ Models/Inventory/Inventory.cs
@@ -10,5 +10,35 @@
         {
             items = inventoryItems;
         }
+
+        public Item GetItem(string itemName)
+        {
+            foreach (Item item in items)
+            {
+                if (item.GetName() == itemName)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+
+        public void AddItem(Item item)
+        {
+            items.Add(item);
+        }
+
+        public void RemoveItem(string itemName) { items.Remove(GetItem(itemName)); }
+
+        public void RemoveAllItems() { items.Clear(); }
+
+        // Getters and setters for class variables - no other methods beyond this point
+
+        public List<Item> GetItems() { return items; }
+
+        public void SetItems(List<Item> inventoryItems) 
+        { 
+            items = inventoryItems; 
+        }
     }
 }
