af5e1e4da675a83d0f9e6aa4b3fb184b54bd3380
Matthew Akrigg <matthewakrigg05@gmail.com>
2025-03-02 14:00:22 +0000

    feat: person gets/sets and db items class

--- Models/People/Person.cs
+++ Models/People/Person.cs
@@ -16,5 +16,63 @@
             contactNumber = contactNum;
             emailAddress = emailAdd;
         }
+
+        protected Person(string fname, string contactNum, string emailAdd)
+        {
+            firstName = fname;
+            contactNumber = contactNum;
+            emailAddress = emailAdd;
+        }
+
+        public void SetFirstName(string fname)
+        {
+            firstName = fname;
+        }
+
+        public string GetFirstName()
+        {
+            return firstName;
+        }
+
+        public void SetLastName(string lname)
+        {
+            lastName = lname;
+        }
+
+        public string GetLastName()
+        {
+            return lastName;
+        }
+
+        public void SetContactNumber(string contactNum)
+        {
+            contactNumber = contactNum;
+        }
+
+        public string GetContactNumber()
+        {
+            return contactNumber;
+        }
+
+        public void SetEmailAddress(string emailAdd)
+        {
+            emailAddress = emailAdd;
+        }
+
+        public string GetEmailAddress()
+        {
+            return emailAddress;
+        }
+
+        public string GetFullName()
+        {
+            return $"{firstName} {lastName}";
+        }
+
+        public string GetContactInfo()
+        {
+            return $"Contact: {contactNumber} | Email: {emailAddress}";
+        }
+
     }
 }
--- Utils/DBHelper.cs
+++ Utils/DBHelper.cs
@@ -5,7 +5,6 @@ namespace Utils
 {
     public static class DBHelper
     {
-
         public static async Task<MySqlConnection> GetConnection()
         {
             string dbPass = Environment.GetEnvironmentVariable("dbPass") ?? throw new InvalidOperationException("Database password environment variable is not set.");
