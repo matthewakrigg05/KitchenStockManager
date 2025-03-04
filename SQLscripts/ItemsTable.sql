220de2ab66e5a8d61bb5af8efa36a7bf3e9c1e1b
Matthew Akrigg <matthewakrigg05@gmail.com>
2025-03-04 21:06:32 +0000

    feat: items table

---
+++ SQLscripts/ItemsTable.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE Items (
+    itemId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(250) NOT NULL,
+    quantity INT NOT NULL,
+    unit VARCHAR(50) NOT NULL,
+    cost FLOAT,
+    supplierId INT,
+    FOREIGN KEY (supplierId) REFERENCES Suppliers(supplierId)
+);
