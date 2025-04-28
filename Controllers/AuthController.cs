310e1b4ce6121460e4e52e7bfa5a7c4c39093cfe
Matthew Akrigg <matthewakrigg05@gmail.com>
2025-04-28 15:52:41 +0100

    feat: api auth controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using KitchenStockManager.Models.People.Users;
+
+namespace KitchenStockManager.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest loginRequest)
+        {
+            var user = await UserManager.LogIn(loginRequest.Email, loginRequest.Password);
+
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            return Ok(new { message = "Login successful", user = user.getEmail() });
+        }
+    }
+}
+
