00de7b5eb0b610697df05a3c8daae95159d5a9e1
Matthew Akrigg <matthewakrigg05@gmail.com>
2025-04-28 15:54:54 +0100

    feat: api auth controller

---
+++ Models/People/Users/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace KitchenStockManager.Models.People.Users
+{
+    public class LoginRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
