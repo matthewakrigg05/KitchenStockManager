2aab4b5b5273d5d9e51094383a8681d459969190
Matthew <matthewakrigg05@gmail.com>
2025-06-13 19:33:01 +0100

    feat: menu and dish tables

--- SQLscripts/Inventory.sql
+++ SQLscripts/Inventory.sql
@@ -1,14 +1,14 @@
 CREATE TABLE InventoryItems (
     inventoryItemId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    itemType ENUM('RawIngredient', 'PreparedIngredient', 'Item') NOT NULL,
+    itemType ENUM('Raw', 'Prepared', 'Item') NOT NULL,
     itemId INT NOT NULL,
     quantity INT NOT NULL,
     unitOfMeasure VARCHAR(50) NOT NULL,
     costPerUnit DECIMAL(10,2) NOT NULL,
     FOREIGN KEY (itemId) REFERENCES (
         CASE
-            WHEN itemType = 'RawIngredient' THEN RawIngredients(rawIngredientId)
-            WHEN itemType = 'PreparedIngredient' THEN PreparedIngredients(preparedIngredientId)
+            WHEN itemType = 'Raw' THEN RawIngredients(rawIngredientId)
+            WHEN itemType = 'Prepared' THEN PreparedIngredients(preparedIngredientId)
             WHEN itemType = 'Item' THEN Items(itemId)
         END
     )
