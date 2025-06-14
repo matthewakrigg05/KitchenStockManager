26c53091217edca75be852c47af6fc5c810fcb77
Matthew <matthewakrigg05@gmail.com>
2025-06-14 23:31:15 +0100

    refactor: separating methods in db init, then added check for db existing, and updated program to check db on start

--- KitchenStockManager/Services/DatabaseServices/DatabaseInit.cs
+++ KitchenStockManager/Services/DatabaseServices/DatabaseInit.cs
@@ -3,33 +3,48 @@ using Utils;
 
 public class DatabaseInitialiser
 {
-    public SqliteConnection DatabaseConnection;
-    public string[] TableCreationStatements;
+    private readonly string _tableScriptPath;
+    public List<string> TableCreationStatements { get; private set; } = new List<string>();
 
-    public DatabaseInitialiser()
-    {}
-
-    public void sqlTableCreation(string filePath)
+    public DatabaseInitialiser(string tableScriptPath)
     {
-        string sqlFile = File.ReadAllText(filePath);
+        _tableScriptPath = tableScriptPath;
+    }
+
+    public void ExtractSqlTableStatements()
+    {
+        string sqlFile = File.ReadAllText(_tableScriptPath);
         string[] sqlStatements = sqlFile.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
 
+        foreach (string statement in sqlStatements)
+        {
+            string trimmed = statement.Trim();
+
+            if (!string.IsNullOrWhiteSpace(trimmed))
+            {
+                TableCreationStatements.Add(trimmed);
+            }
+        }
+    }
+
+    public void CreateSqlTables()
+    {
         using (var conn = DBHelper.GetConnection())
         {
-            foreach (string statement in sqlStatements)
+            foreach (string statement in TableCreationStatements)
             {
-                string trimmedStatement = statement.Trim();
-                if (!string.IsNullOrWhiteSpace(trimmedStatement))
-                {
-                    using (var cmd = new SqliteCommand(trimmedStatement, conn))
-                    {
-                        cmd.ExecuteNonQuery();
-                    }
-                }
+                using (var cmd = new SqliteCommand(statement, conn))
+                    cmd.ExecuteNonQuery();
             }
 
             Console.WriteLine("SQL tables have been successfully created!");
         }
-    
+    }
+
+    // As part of the db initialiser, it has to check if the db exists first, if so, skip initialisation to avoid sql errors for tables
+    // that already exists
+    public bool DatabaseExists(string dbPath="KSM.db")
+    {
+        return File.Exists(dbPath);
     }
 }
